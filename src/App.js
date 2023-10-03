d287d83098312c2007789a7bfa9c9803907ab232
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2023-10-03 14:20:15 +0300

    Lab 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
-// Практическая работа
 import React from "react";
-import Profile from "./practice/profile";
-import Congratulations from "./practice/congratulations";
-import TodoList from "./practice/person";
-import PackingList from "./practice/list"
-import LightSwitch from "./practice/light_switch";
-import Signup from "./practice/signup";
-import Toolbar from "./practice/toolbar";
-import Form from "./practice/form";
-import FeedbackForm from "./practice/feedback";
+import User from "./components/user";
+import List from "./components/list";
+import Person from "./components/person";
+import Product from "./components/product";
+import Counter from "./components/counter";
 
 function App() {
+  // Данные для компонентов
+  const userData = {
+    name: "Иван Иванов",
+    age: 30,
+  };
+
+  const itemList = [
+    { name: "Элемент 1" },
+    { name: "Элемент 2" },
+    { name: "Элемент 3" },
+    { name: "Элемент 4" },
+  ];
+
+  const personData = {
+    name: "Анна Смирнова",
+    age: 25,
+    email: "anna@example.com",
+  };
+
+  const productData = {
+    name: "Продукт 1",
+    price: 100,
+    description: "Описание продукта 1",
+  };
+
+  // Состояние счетчика
+  const [counter, setCounter] = React.useState(0);
+
+  const handleCounterIncrement = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
-      <Profile />
-      <Congratulations/>
-      <TodoList />
-      <PackingList/>
-      <LightSwitch/>
-      <Signup/>
-      <Toolbar/>
-      <Form/>
-      <FeedbackForm/>
+      <h1>Примеры компонентов React</h1>
+
+      {/* Пример использования компонента User */}
+      <User name={userData.name} age={userData.age} />
+
+      {/* Пример использования компонента List */}
+      <List items={itemList} />
+
+      {/* Пример использования компонента Person */}
+      <Person {...personData} />
+
+      {/* Пример использования компонента Product */}
+      <Product {...productData} />
+
+      {/* Пример использования компонента Counter */}
+      <Counter count={counter} onIncrement={handleCounterIncrement} />
     </div>
   );
 }
