ede78f00c116907c0cd955c00467e7ae7d2a9bda
Maru <osmanov.m.i.20@gmail.com>
2023-12-12 06:03:58 +0300

    Lab 4.3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { useState } from 'react';
-import Modal from './Modal';
+import React from 'react';
+import useInput from './components/useInput';
 import './App.css'
 
 const App = () => {
-    const [modalVisible, setModalVisible] = useState(false);
-
-    const openModal = () => {
-        setModalVisible(true);
-    };
-
-    const closeModal = () => {
-        setModalVisible(false);
-    };
+    const nameInput = useInput('', true); // Поле обязательно для заполнения
 
     return (
         <div>
-            <button onClick={openModal}>Открыть модальное окно</button>
-
-            <Modal show={modalVisible} onClose={closeModal}>
-                <h1>Привет, это модальное окно!</h1>
-                <button onClick={closeModal}>Закрыть</button>
-            </Modal>
+            <label>
+                Имя:
+                <input
+                    type="text"
+                    value={nameInput.value}
+                    onChange={nameInput.onChange}
+                    onBlur={nameInput.onBlur}
+                />
+                <div>
+                    {nameInput.error && <span>{nameInput.error}</span>}
+                </div>
+            </label>
         </div>
     );
 };
--- src/components/useInput.js
+++ src/components/useInput.js
@@ -1,18 +1,16 @@
-import {useState} from 'react';
+import {useState} from 'react'
 
 const useInput = (initial, required) => {
-    const [value, setValue] = useState(initial);
-    const [error, setError] = useState(null);
-
+    const [value, setValue] = useState(initial)
+    const [error, setError] = useState(null)
     return {
         value,
         error,
-        onChange: (e) => setValue(e.target.value),
-        onBlur: () => {
-            if (!value && required) setError("Required field");
-            else setError(null);
-        },
-    };
-};
-
+        onChange: e => setValue(e.target.value),
+        onBlur: e => {
+            if (!e.target.value && required) setError("Required field")
+            else setError(null)
+        }
+    }
+}
 export default useInput;
