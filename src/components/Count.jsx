d91c0cb8c87e6130d95805e507373431f42b9801
Maru <osmanov.m.i.20@gmail.com>
2023-12-12 14:40:31 +0300

    Lab 4.1 fix

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const render = {
-  count1: 0,
-  count2: 0,
+    count1: 0,
+    count2: 0,
 };
 
-const Count = ({ id, value }) => {
-  console.warn(`🔴 Count${id} render: ${++render[`count${id}`]}`);
+const Count = ({id, value}) => {
+    console.log('asdfsadf')
+    console.warn(`🔴 Count${id} render: ${++render[`count${id}`]}`);
 
-  return (
-      <div>
-        <h1 className="cyan">{value}</h1>
-      </div>
-  );
+    return (
+        <div>
+            <h1 className="cyan">{value}</h1>
+        </div>
+    );
 };
 
 export default React.memo(Count);
--- src/components/IsFive.jsx
+++ src/components/IsFive.jsx
@@ -1,17 +1,17 @@
-import React, { useMemo } from 'react';
+import React, {useMemo} from 'react';
 
 let renderCount = 0;
 
-const IsFive = ({ value }) => {
-  console.warn(`🔴 isFive render: ${++renderCount}`);
+const IsFive = ({value}) => {
+    console.warn(`🔴 isFive render: ${++renderCount}`);
 
-  const getResult = useMemo(() => {
-    let i = 0;
-    while (i < 600000000) i++;
-    return value === 5 ? '✅ Это пять :D' : '❌ Это не пять :(';
-  }, [value]);
+    const getResult = useMemo(() => {
+        let i = 0;
+        while (i < 600000000) i++;
+        return value === 5 ? '✅ Это пять :D' : '❌ Это не пять :(';
+    }, [value]);
 
-  return <h3>{getResult}</h3>;
+    return <h3>{getResult}</h3>;
 };
 
-export default React.memo(IsFive);
+export default IsFive;
