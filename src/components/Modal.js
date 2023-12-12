b22e9e17bd0820e080246944be0a72a6a08e7306
Maru <osmanov.m.i.20@gmail.com>
2023-12-12 17:23:23 +0300

    Lab 4.2 modal window fix

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,22 @@
 // Modal.js
 import React from 'react';
+import './Modal.css'
 
-const Modal = ({onClose, onConfirm }) => {
+const Modal = ({ onClose, onConfirm }) => {
+    const handleOverlayClick = (e) => {
+        if (e.target.classList.contains('modal-overlay')) {
+            onClose();
+        }
+    };
+
     return (
+        <div className="modal-overlay" onClick={handleOverlayClick}>
             <div className="modal-content">
                 <p>Вы действительно хотите удалить фигуру?</p>
                 <button onClick={onConfirm}>Да</button>
                 <button onClick={onClose}>Отмена</button>
             </div>
+        </div>
     );
 };
 
